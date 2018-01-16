b7a0fda6a327f564d4580d31e5225e4a50613491
Monikanikaa <1nika11@wp.pl>
2018-01-16 21:30:53 +0100

    added while added some variables changed some variables

--- higher_lower.py
+++ higher_lower.py
@@ -8,6 +8,10 @@ runda = int(input("Określ liczbę rund: "))
 start_zakres = int(input("Wprowadź liczbę stanowiącą początek przedziału liczbowego: "))
 end_zakres = int(input("Wprowadź liczbę stanowiącą koniec przedziału liczbowego: "))
 
+while end_zakres < start_zakres:
+    print("Liczba z końca przedziału musi być większa od liczby z początku przedziału!")
+    end_zakres = int(input("Wprowadź liczbę stanowiącą koniec przedziału liczbowego: "))
+
 liczba = random.randrange(start_zakres, end_zakres)
 
 
@@ -15,15 +19,26 @@ for i in range(runda):
     wybor = int(input("Odgadnij liczbę: "))
     print("Liczba, którą wybrałeś to: ", wybor)
 
+    odpowiedz = ""
+    jeszcze_raz = "Spróbuj jeszcze raz ;)"
+
     if wybor < liczba:
-        print("Podana liczba jest ZA MAŁA! \nSpróbuj jeszcze raz ;)")
+        odpowiedz = "Podana liczba jest ZA MAŁA!"
+        print(odpowiedz)
+        if not i == runda - 1:
+            print(jeszcze_raz)
 
     elif wybor > liczba:
-        print("Podana liczba jest ZA DUŻA! \n Spróbuj jeszcze raz ;)")
+        odpowiedz = "Podana liczba jest ZA DUŻA!"
+        print(odpowiedz)
+        if not i == runda - 1:
+            print(jeszcze_raz)
 
     elif wybor == liczba:
         print("Brawo, zgadłeś/aś! Odgadnięta liczba to: ", liczba)
+        
+    if i == runda - 1:
+        print("\n\nNiestety nie udało Ci się odgadnąć wylosowanej liczby :( \n"
+              "Zagraj jeszcze raz!")
 
-print("\n\nNiestety nie udało Ci się odgadnąć wylosowanej liczby :( \n"
-      "Zagraj jeszcze raz!")
-
+input("Aby zakończyć, wciśnij ENTER")
